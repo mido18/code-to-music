c32de64e0e0162e8031c73c74002ae236d1d85bc
mohamed abdelrahman mohamed <mohamed_abdelrahman@icloud.com>
2025-07-24 19:58:42 +0300

    adding app based routes and lamejs

---
+++ src/app/api/checkout/route.ts
@@ -0,0 +1,41 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: '2025-06-30.basil',
+});
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  try {
+    const origin = req.headers.get('origin');
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price: 'price_1RoNTA9HcTSLr7iBMv1ahEjs',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${origin}/?success=true`,
+      cancel_url: `${origin}/?cancel=true`,
+    });
+
+    return new Response(JSON.stringify({ url: session.url }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    console.error('Checkout Error:', error);
+
+    return new Response(JSON.stringify({ error: 'Payment failed' }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+}
