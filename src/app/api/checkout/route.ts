7eb7315c04cd051ce95dfa6d671909eba469d15d
mohamed abdelrahman mohamed <mohamed_abdelrahman@icloud.com>
2025-07-25 23:43:03 +0300

    fix for deployment

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,14 +1,14 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2025-06-30.basil',
 });
 
-export async function POST(req: NextRequest) {
+export async function POST(request: Request) {
   try {
-    const origin = req.headers.get('origin');
-    const { userId } = (await req.json()) as { userId: string };
+    const origin = request.headers.get('origin');
+    const { userId } = (await request.json()) as { userId: string };
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
